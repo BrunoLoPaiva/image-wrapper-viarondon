f2df67a775fedacfc338372aab60d35bc27ed2d8
Pedro Lucas Pereira Flores <pedro.flores@VIARONDON.LOCAL>
2024-04-29 14:40:44 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+import Wrapper from './pages/Wrapper';
+
+function App() {
+
+
+  return (
+    <>
+      <Wrapper/>
+    </>
+  );
+}
+
+export default App;
